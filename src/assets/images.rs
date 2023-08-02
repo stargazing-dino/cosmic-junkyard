f126a0e087072b76274385cedb7d1306562153cc
Rex Magana <Rex.magana@gmail.com>
2023-08-01 20:02:55 -0700

    remove last warnings

--- src/assets/images.rs
+++ src/assets/images.rs
@@ -1,6 +1,4 @@
-use bevy::asset::AssetServer;
-use bevy::prelude::Image;
-use bevy::prelude::{Handle, Resource};
+use bevy::prelude::Resource;
 use bevy_asset_loader::prelude::*;
 
 #[derive(AssetCollection, Resource)]
